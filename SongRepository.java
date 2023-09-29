468de16015c1f70e6dd70ef1c0337cd1dac0cf4a
Valeria Goya <116750605+valgc263@users.noreply.github.com>
2023-09-29 09:54:31 -0500

    Delete SongRepository.java

--- SongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoo.song;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-    List<Song> findByName(String name);
-}
